666c35007984fa86cb2626980ab491ac7b62b932
Alan Jian <alanjian85@outlook.com>
2023-02-11 22:33:07 +0800

    Load timer in the main program

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <signal.h>
 #include <stdio.h>
+#include <unistd.h>
 
 void alrmhandler(int) {
     static int count = 0;
@@ -8,5 +9,13 @@ void alrmhandler(int) {
 
 int main() {
     signal(SIGALRM, alrmhandler);
+
+    pid_t pid = getpid();
+    if (fork() == 0) {
+        char arg[16];
+        sprintf(arg, "%d", pid);
+        execl("timer", "timer", arg, NULL);
+    }
+
     for (;;);
 }
